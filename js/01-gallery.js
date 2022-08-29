f45460de0eaeb397f6ad614b030e0652b7634c27
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-29 14:04:42 +0300

    refactoring the first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -32,27 +32,80 @@ function onImageClick(e) {
   }
 
   createsBasicLightboxInstance(e);
-
-  checkEscapeKeydown();
 }
 
 function createsBasicLightboxInstance(e) {
-  basicLightbox
-    .create(
-      `
+  const instance = basicLightbox.create(
+    `
     <img src="${e.target.dataset.source}" width="800" height="600">
 `
-    )
-    .show();
-}
+  );
 
-function checkEscapeKeydown() {
-  document.addEventListener('keydown', onEscapeKeydown);
+  instance.show(() => document.addEventListener('keydown', onEscapeKeydown));
 
   function onEscapeKeydown(e) {
     if (e.code === 'Escape') {
-      document.querySelector('.basicLightbox')?.remove();
-      document.removeEventListener('keydown', onEscapeKeydown);
+      instance.close(() => document.removeEventListener('keydown', onEscapeKeydown));
     }
   }
 }
+
+// ** v2
+// import { galleryItems } from './gallery-items.js';
+
+// const gallery = document.querySelector('.gallery');
+// const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+// gallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+// gallery.addEventListener('click', onImageClick);
+
+// function createGalleryItemsMarkup(images) {
+//   return images.reduce(
+//     (acc, { preview, original, description }) =>
+//       acc +
+//       `<div class="gallery__item">
+//           <a class="gallery__link" href="${original}">
+//             <img
+//               class="gallery__image"
+//               src="${preview}"
+//               data-source="${original}"
+//               alt="${description}"
+//             />
+//           </a>
+//         </div>`,
+//     ''
+//   );
+// }
+
+// function onImageClick(e) {
+//   e.preventDefault();
+
+//   if (e.target.nodeName !== 'IMG') {
+//     return;
+//   }
+
+//   createsBasicLightboxInstance(e);
+
+//   checkEscapeKeydown();
+// }
+
+// function createsBasicLightboxInstance(e) {
+//   basicLightbox
+//     .create(
+//       `
+//     <img src="${e.target.dataset.source}" width="800" height="600">
+// `
+//     )
+//     .show();
+// }
+
+// function checkEscapeKeydown() {
+//   document.addEventListener('keydown', onEscapeKeydown);
+
+//   function onEscapeKeydown(e) {
+//     if (e.code === 'Escape') {
+//       document.querySelector('.basicLightbox')?.remove();
+//       document.removeEventListener('keydown', onEscapeKeydown);
+//     }
+//   }
+// }
